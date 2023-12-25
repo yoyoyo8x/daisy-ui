c5c1ef5e40b299d617c7ba54c712486e36c0b4dc
yoyoyo8x <ductrung8697@gmail.com>
2023-12-25 08:47:13 +0700

    remove theme

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,7 +17,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" data-theme="winter">
+    <html lang="en">
       <body className={inter.className}>
         <ReduxProvider>
           <Header />
